8146fe30639b10ebfc6fdbab76529cb766b01a52
Bin800 <theday539@gmail.com>
2026-09-13 22:24:37 +0700

    feat: add lesson-02 folder

---
+++ tests/lesson-02/ex1.js
@@ -0,0 +1,3 @@
+const a = 12;
+const name = "my number";
+const isEven = false;
---
+++ tests/lesson-02/ex2.js
@@ -0,0 +1,3 @@
+let myName = "Alex"; // Sửa thành biến động để có thể gán giá trị ngc vào
+myName = "Nagi";
+console.log(myName);
